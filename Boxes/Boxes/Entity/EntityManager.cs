f94bd50a51d88ddf4f596e361d26de0e093bdc2a
Ijwu <hmfarran1@hawkmail.hfcc.edu>
2014-12-06 14:24:13 -0500

    Changes (im getting lazy)

--- Boxes/Boxes/Entity/EntityManager.cs
+++ Boxes/Boxes/Entity/EntityManager.cs
@@ -1,15 +1,16 @@
 ﻿using System.Collections.Generic;
+using Boxes.Services;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Boxes.Entity
 {
-    public class EntityManager : Microsoft.Xna.Framework.DrawableGameComponent
+    public class EntityManager : IComponentService
     {
         private List<IEntity> _entities;
         private SpriteBatch _sb;
 
-        public EntityManager(Game game) : base(game)
+        public EntityManager(Game game)
         {
             _entities = new List<IEntity>();
             _sb = new SpriteBatch(game.GraphicsDevice);
@@ -20,20 +21,27 @@ namespace Boxes.Entity
             _entities.Add(ent);
         }
 
-        public override void Draw(GameTime gameTime)
+        public void Draw(GameTime gameTime)
         {
+            _sb.Begin();
             foreach (var ent in _entities)
             {
                 ent.Draw(_sb, gameTime);
             }
+            _sb.End();
         }
 
-        public override void Update(GameTime gameTime)
+        public void Update(GameTime gameTime)
         {
             foreach (var ent in _entities)
             {
                 ent.Update(gameTime);
             }
+        }
+
+        public List<IEntity> GetEntities()
+        {
+            return _entities;
         }
     }
 }
